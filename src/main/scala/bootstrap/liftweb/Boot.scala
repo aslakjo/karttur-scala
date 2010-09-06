7295ecc6aa219f1c51ca12646b481798d12f7615
Aslak Johannessen <aslakjo@bekk.no>
2010-09-06 22:34:00 +0200

    legger til litt stil

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -15,49 +15,25 @@ import code.api._
 import main.scala.code.model.Place
 
 
-/**
- * A class that's instantiated early and run.  It allows the application
- * to modify lift's environment
- */
 class Boot {
   def boot {
     setupDB
 
-    // Use Lift's Mapper ORM to populate the database
-    // you don't need to use Mapper to use Lift... use
-    // any ORM you want
     Schemifier.schemify(true, Schemifier.infoF _, User, Place)
 
-    // where to search snippet
     LiftRules.addToPackages("code")
 
-    // Build SiteMap
     val entries = List(
-      Menu.i("Home") / "index" ,
-      Menu("Map") / "map"
+      Menu.i("Karttur") / "index" ,
+      Menu("Kart") / "map"
     ) 
     
     LiftRules.dispatch.prepend(Api.dispatch)
 
-    // set the sitemap.  Note if you don't want access control for
-    // each page, just comment this line out.
     LiftRules.setSiteMap(SiteMap(entries:_*))
 
-    //Show the spinny image when an Ajax call starts
-    LiftRules.ajaxStart =
-      Full(() => LiftRules.jsArtifacts.show("ajax-loader").cmd)
-    
-    // Make the spinny image go away when it ends
-    LiftRules.ajaxEnd =
-      Full(() => LiftRules.jsArtifacts.hide("ajax-loader").cmd)
-
-    // Force the request to be UTF-8
     LiftRules.early.append(_.setCharacterEncoding("UTF-8"))
 
-    // What is the function to test if a user is logged in?
-    LiftRules.loggedInTest = Full(() => User.loggedIn_?)
-
-    // Make a transaction span the whole HTTP request
     S.addAround(DB.buildLoanWrapper)
   }
 
